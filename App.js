267f9fb51700dac782bfad52c907e0785e37e969
gabkoko <kovacsgbaor70@gmail.com>
2023-12-01 19:26:12 +0100

    V2 style, buttons, pages

--- App.js
+++ App.js
@@ -1,67 +1,170 @@
-import React from "react";
-import { View, Text, StyleSheet } from 'react-native'
+import * as React from 'react';
+import { Button, View, Text, StyleSheet } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 
-export default function App() {
+function HomeScreen({ navigation }) {
   return (
-    <View style={styles.root}><Text style={[styles.text, { textAlign: 'center', textAlignVertical: 'center', fontSize: 36 }]}>Bevásárlólisták</Text>
-      <View
-        style={[
-          styles.container,
-          {
-            backgroundColor: '#e1e4e8',
-          },
-        ]}>
-        {/* zIndex: 0 */}
-        <View style={[styles.item, { backgroundColor: '#5cc9f5', flexDirection: 'row'}]} > 
-        <Text style={[styles.text, { textAlign: 'center', textAlignVertical: 'center' }]}>Tesco</Text></View>  
-        
-        {/* zIndex: 1 */}
-        <View style={[styles.item, { backgroundColor: '#5cc9f5', flexDirection: 'row' }]} >
-        <Text style={[styles.text, { textAlign: 'center', textAlignVertical: 'center' }]}>Lidl</Text></View>  
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'top' }}>
+      <Text> </Text>
+      <Button 
+        title="Tesco"
+        onPress={() => navigation.navigate('Vissza')}
+      />
+      <Text> </Text>
+      <Button
+        title="Penny"
+        onPress={() => navigation.navigate('Vissza2')}
+      />
+      <Text> </Text>
+      <Button
+        title="Lidl"
+        onPress={() => navigation.navigate('Vissza3')}
+      />
+      <Text> </Text>
+      <Button
+        title="Kik"
+        onPress={() => navigation.navigate('Vissza4')}
+      />
+      <Text> </Text>
+      <Button
+        title="Euro Family"
+        onPress={() => navigation.navigate('Vissza5')}
+      />
+      <Text> </Text>
+      <Button
+        title="Interspar"
+        onPress={() => navigation.navigate('Vissza6')}
+      />
 
-        {/* zIndex: 2 */}
-        <View style={[styles.item, { backgroundColor: '#5cc9f5', flexDirection: 'row' }]} >
-        <Text style={[styles.text, { textAlign: 'center', textAlignVertical: 'center' }]}>Penny</Text></View> 
-
-        {/* zIndex: 3 */}
-        <View style={[styles.item, { backgroundColor: '#5cc9f5', flexDirection: 'row' }]} >
-        <Text style={[styles.text, { textAlign: 'center', textAlignVertical: 'center' }]}>KIK</Text></View> 
-
-        {/* zIndex: 4 */}
-        <View style={[styles.item, { backgroundColor: '#5cc9f5', flexDirection: 'row' }]} >
-        <Text style={[styles.text, { textAlign: 'center', textAlignVertical: 'center' }]}>EuroFamily</Text></View>
-
-        {/* zIndex: 5 */}
-        <View style={[styles.item, { backgroundColor: '#5cc9f5', flexDirection: 'row' }]} >
-        <Text style={[styles.text, { textAlign: 'center', textAlignVertical: 'center' }]}>Interspar</Text></View>  
-      </View>
-       
     </View>
- 
+  );
+}
+
+
+////////////////////////////////////////////////////////////////////////////
+
+function DetailsScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'top' }}>
+      <Text> </Text>
+    
+      <Text>Tesco Bevásárló lista</Text>
+
+      
+
+    </View>
+  );
+  
+}
+
+function DetailsScreen2() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'top' }}>
+      <Text> </Text>
+      <Text>Penny Bevásárló lista</Text>
+
+      
+
+    </View>
+  );
+  
+}
+
+function DetailsScreen3() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'top' }}>
+      <Text> </Text>
+      <Text>Lidl Bevásárló lista</Text>
+
+      
+
+    </View>
+  );
+  
+}
+
+function DetailsScreen4() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'top' }}>
+      <Text> </Text>
+      <Text>Kik Bevásárló lista</Text>
+
+      
+
+    </View>
+  );
+  
+}
+
+function DetailsScreen5() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'top' }}>
+      <Text> </Text>
+      <Text>Euro family Bevásárló lista</Text>
+
+      
+
+    </View>
+  );
+  
+}
+
+function DetailsScreen6() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'top' }}>
+      <Text> </Text>
+      <Text>Interspar Bevásárló lista</Text>
+
+      
+
+    </View>
+  );
+  
+}
+
+const Stack = createNativeStackNavigator();
+
+const globalScreeOptions = {
+  headerStyle: { backgroundColor: "#2C6BED" },
+  headerTitleStyle: { color: "white" },
+  headerTintColor: "white",
+};
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home" screenOptions={globalScreeOptions}>
+        <Stack.Screen name="Bevásárlólisták" component={HomeScreen} />
+        <Stack.Screen name="Vissza" component={DetailsScreen} />
+        <Stack.Screen name="Vissza2" component={DetailsScreen2} />
+        <Stack.Screen name="Vissza3" component={DetailsScreen3} />
+        <Stack.Screen name="Vissza4" component={DetailsScreen4} />
+        <Stack.Screen name="Vissza5" component={DetailsScreen5} />
+        <Stack.Screen name="Vissza6" component={DetailsScreen6} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
-  root: {
-    flex: 1,
-    backgroundColor: '#fff000',
+  button: {
     alignItems: 'center',
     justifyContent: 'center',
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+    borderRadius: 4,
+    elevation: 3,
+    backgroundColor: 'black',
   },
-  container: {
-    height: 650,
-    width: 300,
-    borderRadius: 16,
-    padding: 16,
-    borderWidth: 4,
-    borderColor: 'rgba(0,0,0,0.2)',
-  },
-  item: {
-    borderWidth: 4,
-    borderColor: 'rgba(0,0,0,0.2)',
-    height: 96,
-    width: 260,
-    borderRadius: 8,
+  text: {
+    fontSize: 16,
+    lineHeight: 21,
+    fontWeight: 'bold',
+    letterSpacing: 0.25,
+    color: 'white',
   },
 });
+
+export default App;
